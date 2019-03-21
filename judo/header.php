c9c85797d3f7045a3f8488b685cc9479f5f72c05
mfish911 <33205066+mfish911@users.noreply.github.com>
2019-03-21 14:58:01 -0400

    Add files via upload

---
+++ judo/header.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>style.css">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/favicon-16x16.png">
+<link rel="manifest" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/site.webmanifest">
+<link rel="mask-icon" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>media/favicon.ico">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link rel="stylesheet" href="<?php if($thisfolder=="tournament"){echo "../../";} else if (!empty($thisfolder)) {echo "../";} ?>css/bootstrap.css">
+<link href="<?php if($thisfolder=="tournament"){echo "../..";} else if (!empty($thisfolder)) {echo "..";} ?>bootstrap-4-hover-navbar.css" rel="stylesheet">
---
+++ judo/index.php
@@ -0,0 +1,25 @@
+<?php
+$thispage="index.php";
+$thisfolder="";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "header.php";?>
+    <title>Aspire Judo</title>
+</head>
+<body>
+
+<div class="ti">
+    <?php include "topnav.php";?>
+</div>
+<div class="bgo">
+    <div class="c">
+        <h1>Aspire Judo</h1>
+    </div>
+</div>
+<div class="fshort">
+    <?php include "footer.php";?>
+</div>
+</body>
+</html>
